05cf39b6487f054af7d3d13d348dc6fa3fa8f61a
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-09-27 13:36:45 +0300

    Time: 0 ms (100.00%) | Memory: 62.9 MB (78.70%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,19 +4,16 @@ public:
         int left = 0;
         int right = height.size() - 1;
         int max_area = 0;
-
         while (left < right) {
             int h = min(height[left], height[right]);
             int w = right - left;
             max_area = max(max_area, h * w);
-
             if (height[left] < height[right]) {
                 ++left;
             } else {
                 --right;
             }
         }
-
         return max_area;
     }
 };

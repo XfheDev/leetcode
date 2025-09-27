4ec3b3d0caf536c0bbb5cd467e7594a570260a2f
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-09-27 13:33:23 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (75.66%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        vector<bool> dp(n+1, false);
+        dp[0] = true; // boş string boş pattern ile eşleşir
 
-        dp[0][0] = true; // boş string boş pattern ile eşleşir
-
-        // p ile eşleşmeyen baştaki '*' durumlarını ayarlama
+        // İlk satır (i=0) için '*' ile eşleşmeleri ayarla
         for (int j = 2; j <= n; j += 2) {
-            if (p[j-1] == '*') dp[0][j] = dp[0][j-2];
+            if (p[j-1] == '*') dp[j] = dp[j-2];
         }
 
         for (int i = 1; i <= m; ++i) {
+            vector<bool> new_dp(n+1, false);
             for (int j = 1; j <= n; ++j) {
                 if (p[j-1] == s[i-1] || p[j-1] == '.') {
-                    dp[i][j] = dp[i-1][j-1]; // karakterler eşleşiyor
+                    new_dp[j] = dp[j-1];
                 } else if (p[j-1] == '*') {
-                    // '*' önceki karakteri sıfır kez kullanabilir
-                    dp[i][j] = dp[i][j-2];
-                    // veya önceki karakter eşleşiyorsa bir kez daha kullanabilir
+                    new_dp[j] = new_dp[j-2]; // 0 kez
                     if (p[j-2] == s[i-1] || p[j-2] == '.')
-                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                        new_dp[j] = new_dp[j] || dp[j]; // 1+ kez
                 }
             }
+            dp.swap(new_dp); // rolling
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 };

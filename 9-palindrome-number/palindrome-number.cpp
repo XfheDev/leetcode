6d957fafe8012c1eed29c645908c90f2355e5a11
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-09-27 13:31:11 +0300

    Time: 5 ms (21.07%) | Memory: 10.6 MB (22.10%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negatif sayılar ve 0 hariç sonu 0 ile biten sayılar palindrome olamaz
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        // negatif sayı palindrome olmaz
+        if (x < 0) return false;
 
-        int reverted = 0;
-        // Döngü sadece sayının yarısı kadar devam eder
-        while (x > reverted) {
-            reverted = reverted * 10 + x % 10;
-            x /= 10;
+        // tek basamak palindrome
+        if (x < 10) return true;
+
+        string s = to_string(x); // string dönüşü
+        int n = s.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s[i] != s[n - 1 - i]) return false;
         }
-
-        // Branch-minimized comparison
-        return x == reverted || x == reverted / 10;
+        return true;
     }
 };

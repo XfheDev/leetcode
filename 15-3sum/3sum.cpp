17ffa24b6fca79e405f64ac5c3ac4653e48de1f2
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-03 21:09:44 +0300

    Time: 49 ms (57.14%) | Memory: 28.7 MB (99.88%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,25 +6,27 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        if (n < 3) return res;
+
+        sort(nums.begin(), nums.end());
+        res.reserve(n); // sonuç için ön bellek ayır
 
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // aynı sayıyı atla
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // tekrarları atla
 
             int left = i + 1, right = n - 1;
             while (left < right) {
                 long sum = (long)nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    // tekrarları atla
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++; right--;
-                } else if (sum < 0) {
+                if (sum < 0) {
                     left++;
-                } else {
+                } else if (sum > 0) {
                     right--;
+                } else {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    int lastLeft = nums[left], lastRight = nums[right];
+                    while (left < right && nums[left] == lastLeft) left++;
+                    while (left < right && nums[right] == lastRight) right--;
                 }
             }
         }

b54e6fcf98ce0bd912e7d0e4e6cf5e75756f91e7
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-03 21:03:20 +0300

    Time: 32 ms (45.16%) | Memory: 18.4 MB (54.11%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,30 +1,34 @@
 #include <vector>
 #include <queue>
 #include <tuple>
+#include <bitset>
 using namespace std;
 
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) {
         int m = heightMap.size(), n = heightMap[0].size();
-        if (m < 3 || n < 3) return 0; // havuz olamaz
+        if (m < 3 || n < 3) return 0;
 
-        // Min-heap: (yükseklik, i, j)
+        // visited için düz bitset (max m,n = 200 → 40k hücre)
+        static bitset<200*200> visited; 
+        visited.reset();
+
         using T = tuple<int,int,int>;
         priority_queue<T, vector<T>, greater<T>> pq;
 
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-
-        // Kenarları heap'e ekle
+        // Kenarları ekle
         for (int i = 0; i < m; i++) {
             pq.emplace(heightMap[i][0], i, 0);
             pq.emplace(heightMap[i][n-1], i, n-1);
-            visited[i][0] = visited[i][n-1] = true;
+            visited.set(i*n + 0);
+            visited.set(i*n + (n-1));
         }
         for (int j = 1; j < n-1; j++) {
             pq.emplace(heightMap[0][j], 0, j);
             pq.emplace(heightMap[m-1][j], m-1, j);
-            visited[0][j] = visited[m-1][j] = true;
+            visited.set(0*n + j);
+            visited.set((m-1)*n + j);
         }
 
         int water = 0, maxHeight = 0;
@@ -36,8 +40,10 @@ public:
 
             for (auto& d : dirs) {
                 int ni = i + d[0], nj = j + d[1];
-                if (ni < 0 || nj < 0 || ni >= m || nj >= n || visited[ni][nj]) continue;
-                visited[ni][nj] = true;
+                if (ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
+                int idx = ni * n + nj;
+                if (visited[idx]) continue;
+                visited.set(idx);
 
                 int nh = heightMap[ni][nj];
                 if (nh < maxHeight) {

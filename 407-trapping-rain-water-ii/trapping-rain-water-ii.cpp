fb980372fb79b4eb5df6aba3336408c8ce1d9a0f
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-03 21:04:59 +0300

    Time: 21 ms (87.20%) | Memory: 17.3 MB (84.28%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,58 +1,58 @@
 #include <vector>
 #include <queue>
-#include <tuple>
-#include <bitset>
 using namespace std;
 
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) {
-        int m = heightMap.size(), n = heightMap[0].size();
-        if (m < 3 || n < 3) return 0;
+        int m = heightMap.size();
+        if (m == 0) return 0;
+        int n = heightMap[0].size();
+        if (n == 0) return 0;
 
-        // visited için düz bitset (max m,n = 200 → 40k hücre)
-        static bitset<200*200> visited; 
-        visited.reset();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        using Cell = pair<int, pair<int,int>>; 
+        priority_queue<Cell, vector<Cell>, greater<Cell>> pq;
 
-        using T = tuple<int,int,int>;
-        priority_queue<T, vector<T>, greater<T>> pq;
-
-        // Kenarları ekle
+        // Kenar hücrelerini ekle
         for (int i = 0; i < m; i++) {
-            pq.emplace(heightMap[i][0], i, 0);
-            pq.emplace(heightMap[i][n-1], i, n-1);
-            visited.set(i*n + 0);
-            visited.set(i*n + (n-1));
+            pq.push({heightMap[i][0], {i,0}});
+            pq.push({heightMap[i][n-1], {i,n-1}});
+            visited[i][0] = true;
+            visited[i][n-1] = true;
         }
-        for (int j = 1; j < n-1; j++) {
-            pq.emplace(heightMap[0][j], 0, j);
-            pq.emplace(heightMap[m-1][j], m-1, j);
-            visited.set(0*n + j);
-            visited.set((m-1)*n + j);
+        for (int j = 0; j < n; j++) {
+            pq.push({heightMap[0][j], {0,j}});
+            pq.push({heightMap[m-1][j], {m-1,j}});
+            visited[0][j] = true;
+            visited[m-1][j] = true;
         }
 
-        int water = 0, maxHeight = 0;
+        int water = 0;
         int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while (!pq.empty()) {
-            auto [h, i, j] = pq.top(); pq.pop();
-            maxHeight = max(maxHeight, h);
+            auto cell = pq.top(); pq.pop();
+            int height = cell.first;
+            int x = cell.second.first;
+            int y = cell.second.second;
 
             for (auto& d : dirs) {
-                int ni = i + d[0], nj = j + d[1];
-                if (ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
-                int idx = ni * n + nj;
-                if (visited[idx]) continue;
-                visited.set(idx);
+                int nx = x + d[0];
+                int ny = y + d[1];
 
-                int nh = heightMap[ni][nj];
-                if (nh < maxHeight) {
-                    water += maxHeight - nh;
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    // Daha düşükse su tutulur
+                    if (height > heightMap[nx][ny]) {
+                        water += height - heightMap[nx][ny];
+                    }
+                    // Yükseklik güncellenir
+                    pq.push({max(height, heightMap[nx][ny]), {nx, ny}});
                 }
-                pq.emplace(nh, ni, nj);
             }
         }
-
         return water;
     }
 };
+

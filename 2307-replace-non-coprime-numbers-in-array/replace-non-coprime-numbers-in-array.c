febd9d9f5c558d6dd8b0ef62fc07af8288524c09
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-11 11:19:43 +0300

    Time: 10 ms (76.84%) | Memory: 59.9 MB (5.06%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.c
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.c
@@ -1,31 +1,34 @@
-long long gcd(long long a, long long b) {
+static inline long long gcd(long long a, long long b) {
     while (b) {
-        long long t = b;
-        b = a % b;
-        a = t;
+        long long t = a % b;
+        a = b;
+        b = t;
     }
     return a;
 }
 
-long long lcm(long long a, long long b) {
-    return a / gcd(a, b) * b;
-}
-
 int* replaceNonCoprimes(int* nums, int numsSize, int* returnSize) {
-    int* stack = (int*)malloc(numsSize * sizeof(int));
+    long long* stack = (long long*)malloc(numsSize * sizeof(long long));
     int top = 0;
     
     for (int i = 0; i < numsSize; i++) {
         long long cur = nums[i];
         
-        while (top > 0 && gcd(stack[top - 1], cur) > 1) {
-            cur = lcm(stack[top - 1], cur);
-            top--;
+        while (top) {
+            long long g = gcd(stack[top - 1], cur);
+            if (g == 1) break;
+            cur = stack[--top] / g * cur;
         }
         
         stack[top++] = cur;
     }
     
+    int* result = (int*)malloc(top * sizeof(int));
+    for (int i = 0; i < top; i++) {
+        result[i] = stack[i];
+    }
+    free(stack);
+    
     *returnSize = top;
-    return stack;
+    return result;
 }

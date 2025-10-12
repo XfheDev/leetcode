a81d07a6e054bdaead76e54d9f9c8b0c41b1de19
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-13 00:14:46 +0300

    Time: 2 ms (70.87%) | Memory: 9.5 MB (25.23%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.c
+++ 794-swim-in-rising-water/swim-in-rising-water.c
@@ -1,7 +1,6 @@
 #include <stdbool.h>
 
 #define MAXN 55
-#define MOD 1000000007
 
 typedef struct {
     int h, x, y;
@@ -41,15 +40,11 @@ static inline Node heap_pop(Heap *h) {
 }
 
 int swimInWater(int** grid, int n, int* gridColSize) {
-    static bool vis[MAXN][MAXN];
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            vis[i][j] = false;
-
+    bool vis[MAXN][MAXN] = {0};
     Heap pq = { .sz = 0 };
     heap_push(&pq, (Node){grid[0][0], 0, 0});
     vis[0][0] = true;
-    int maxH = 0;
+    int maxH = grid[0][0];
 
     const int dx[4] = {1, -1, 0, 0};
     const int dy[4] = {0, 0, 1, -1};
@@ -57,16 +52,15 @@ int swimInWater(int** grid, int n, int* gridColSize) {
     while (pq.sz) {
         Node cur = heap_pop(&pq);
         if (cur.h > maxH) maxH = cur.h;
-        if (cur.x == n - 1 && cur.y == n - 1) return maxH;
+        if (cur.x == n-1 && cur.y == n-1) return maxH;
 
-        for (int d = 0; d < 4; ++d) {
-            int nx = cur.x + dx[d];
-            int ny = cur.y + dy[d];
+        for (int d = 0; d < 4; d++) {
+            int nx = cur.x + dx[d], ny = cur.y + dy[d];
             if ((unsigned)nx >= (unsigned)n || (unsigned)ny >= (unsigned)n) continue;
             if (vis[nx][ny]) continue;
             vis[nx][ny] = true;
             heap_push(&pq, (Node){grid[nx][ny], nx, ny});
         }
     }
-    return maxH; 
+    return maxH;
 }

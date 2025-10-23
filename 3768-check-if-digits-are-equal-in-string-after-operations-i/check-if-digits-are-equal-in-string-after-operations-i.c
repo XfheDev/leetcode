ebd646f182e5a7a8c2dc2fd8c2f0be81d0f48243
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-10-23 21:11:16 +0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.82%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.c
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.c
@@ -3,19 +3,16 @@
 
 bool hasSameDigits(char* s) {
     int n = strlen(s);
-    int digits[100];
+    int digits[105];
 
-    // string → sayı dizisine dönüştür
     for (int i = 0; i < n; ++i)
         digits[i] = s[i] - '0';
 
-    // işlemleri aynı dizi üzerinde yap
     while (n > 2) {
         for (int i = 0; i < n - 1; ++i)
             digits[i] = (digits[i] + digits[i + 1]) % 10;
         n--;
     }
 
-    // son iki eleman eşit mi?
     return digits[0] == digits[1];
 }

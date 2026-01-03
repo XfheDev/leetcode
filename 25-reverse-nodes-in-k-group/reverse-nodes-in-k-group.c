7269b6308f1fd455afa8874175597dd37c44a5b3
XfheDev <88056186+XfheDev@users.noreply.github.com>
2026-01-03 19:46:15 +0300

    Time: 0 ms (100.00%) | Memory: 11.4 MB (70.89%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.c
@@ -0,0 +1,33 @@
+struct ListNode* reverseKGroup(struct ListNode* head, int k) {
+    if (head == NULL || k == 1) return head;
+
+
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode *prev = &dummy, *curr = &dummy, *next = &dummy;
+
+
+    int count = 0;
+    while (curr->next != NULL) {
+        curr = curr->next;
+        count++;
+    }
+
+
+    while (count >= k) {
+        curr = prev->next;
+        next = curr->next;
+        
+        for (int i = 1; i < k; i++) {
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+            next = curr->next;
+        }
+        
+        prev = curr;
+        count -= k;
+    }
+
+    return dummy.next;
+}//1 saatimi niye buna verdim lan
